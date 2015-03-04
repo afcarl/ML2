33c8aa55a0664912fac916afac8c39ca78372154
y0ast <joost@joo.st>
2015-03-04 11:16:36 +0100

    omvorm naar python script

---
+++ lab2/Node.py
@@ -0,0 +1,218 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Node(object):
+    """
+    Base-class for Nodes in a factor graph. Only instantiate sub-classes of Node.
+    """
+    def __init__(self, name):
+        # A name for this Node, for printing purposes
+        self.name = name
+        
+        # Neighbours in the graph, identified with their index in this list.
+        # i.e. self.neighbours contains neighbour 0 through len(self.neighbours) - 1.
+        self.neighbours = []
+        
+        # Reset the node-state (not the graph topology)
+        self.reset()
+        
+    def reset(self):
+        # Incoming messages; a dictionary mapping neighbours to messages.
+        # That is, it maps  Node -> np.ndarray.
+        self.in_msgs = {}
+        
+        # A set of neighbours for which this node has pending messages.
+        # We use a python set object so we don't have to worry about duplicates.
+        self.pending = set([])
+
+    def add_neighbour(self, nb):
+        self.neighbours.append(nb)
+
+    def send_sp_msg(self, other):
+        # To be implemented in subclass.
+        raise Exception('Method send_sp_msg not implemented in base-class Node')
+   
+    def send_ms_msg(self, other):
+        # To be implemented in subclass.
+        raise Exception('Method send_ms_msg not implemented in base-class Node')
+    
+    def receive_msg(self, other, msg):
+        # Store the incoming message, replacing previous messages from the same node
+        self.in_msgs[other] = msg
+
+        #Different implementation for loopy and non-loopy propagation
+        # self.pending.update(...)
+        
+        #loopy propagation
+#         pending_neighbours = list(self.neighbours)
+#         pending_neighbours.remove(other)
+#         self.pending.update(pending_neighbours)
+        
+        #non-loopy propagation
+        #BAD code - refactor
+        for neighbour in self.neighbours:
+            if neighbour in self.in_msgs.keys() and len(self.in_msgs) == len(self.neighbours):
+                self.pending.update([neighbour])
+            elif neighbour not in self.in_msgs.keys() and len(self.in_msgs) == (len(self.neighbours) - 1):
+                self.pending.update([neighbour])
+    
+    def __str__(self):
+        # This is printed when using 'print node_instance'
+        return self.name
+
+
+class Variable(Node):
+    def __init__(self, name, num_states):
+        """
+        Variable node constructor.
+        Args:
+            name: a name string for this node. Used for printing. 
+            num_states: the number of states this variable can take.
+            Allowable states run from 0 through (num_states - 1).
+            For example, for a binary variable num_states=2,
+            and the allowable states are 0, 1.
+        """
+        self.num_states = num_states
+        
+        # Call the base-class constructor
+        super(Variable, self).__init__(name)
+    
+    def set_observed(self, observed_state):
+        """
+        Set this variable to an observed state.
+        Args:
+            observed_state: an integer value in [0, self.num_states - 1].
+        """
+        # Observed state is represented as a 1-of-N variable
+        # Could be 0.0 for sum-product, but log(0.0) = -inf so a tiny value is preferable for max-sum
+        self.observed_state[:] = 0.000001
+        self.observed_state[observed_state] = 1.0
+        
+    def set_latent(self):
+        """
+        Erase an observed state for this variable and consider it latent again.
+        """
+        # No state is preferred, so set all entries of observed_state to 1.0
+        # Using this representation we need not differentiate between observed and latent
+        # variables when sending messages.
+        self.observed_state[:] = 1.0
+        
+    def reset(self):
+        super(Variable, self).reset()
+        self.observed_state = np.ones(self.num_states)
+        
+    def marginal(self, Z=None):
+        """
+        Compute the marginal distribution of this Variable.
+        It is assumed that message passing has completed when this function is called.
+        Args:
+            Z: an optional normalization constant can be passed in. If None is passed, Z is computed.
+        Returns: marginal, Z. The first is a numpy array containing the normalized marginal distribution.
+         Z is either equal to the input Z, or computed in this function (if Z=None was passed).
+        """
+        # TODO: compute marginal
+        assert(len(self.in_msgs) == len(self.neighbours))
+        
+        msg_product = np.array([1,1])
+        for key,value in self.in_msgs.iteritems():
+            msg_product *= value
+        
+        #Check if marginalizing goes right
+#         value = np.tensordot(self.f,msg_product, axes=axes)
+        value = 0
+        
+        #compute Z
+
+        
+        return value, Z
+    
+    def send_sp_msg(self, other):
+        # TODO: implement Variable -> Factor message for sum-product
+        if len(self.neighbours) == 1:
+            other.receive_msg(self,np.array([1.,1.]))
+            return
+            
+        received = dict(self.in_msgs)
+        if other in received:
+            del received[other]
+
+        assert(len(received) == (len(self.neighbours) - 1)), "Not all necessary messages have been received"
+            
+        
+#         msgs = [value for key,value in received.iteritems()]
+#         msg_product = np.multiply.reduce(np.ix_(*msgs))
+
+#         msg_product = np.array([1,1])
+#         for key,value in received.iteritems():
+#             msg_product *= value
+            
+        msg_product = np.multiply.reduce(received.values())
+        
+        other.receive_msg(self,msg_product)
+        self.pending.remove(other)
+            
+       
+    def send_ms_msg(self, other):
+        # TODO: implement Variable -> Factor message for max-sum
+        pass
+
+class Factor(Node):
+    def __init__(self, name, f, neighbours):
+        """
+        Factor node constructor.
+        Args:
+            name: a name string for this node. Used for printing
+            f: a numpy.ndarray with N axes, where N is the number of neighbours.
+               That is, the axes of f correspond to variables, and the index along that axes corresponds to a value of that variable.
+               Each axis of the array should have as many entries as the corresponding neighbour variable has states.
+            neighbours: a list of neighbouring Variables. Bi-directional connections are created.
+        """
+        # Call the base-class constructor
+        super(Factor, self).__init__(name)
+
+        assert len(neighbours) == f.ndim, 'Factor function f should accept as many arguments as this Factor node has neighbours'
+        
+        for nb_ind in range(len(neighbours)):
+            nb = neighbours[nb_ind]
+            assert f.shape[nb_ind] == nb.num_states, 'The range of the factor function f is invalid for input %i %s' % (nb_ind, nb.name)
+            self.add_neighbour(nb)
+            nb.add_neighbour(self)
+
+        self.f = f
+        
+    def send_sp_msg(self, other):
+        # TODO: implement Factor -> Variable message for sum-product
+        
+        #Check if all messages from all neighbors except other have been received
+        received = dict(self.in_msgs)
+        if other in received:
+            del received[other]
+    
+        assert(len(received) == (self.f.ndim - 1)), "Not all necessary messages have been received"
+            
+        #Take the product of incoming messages of all other variables
+        msgs = [value for key,value in received.iteritems()]
+        msg_product = np.multiply.reduce(np.ix_(*msgs))
+        
+        #multiply by the factor associated with that node
+        axes = [self.neighbours.index(rec) for rec in received]
+        
+        if len(axes) == 0:
+            axes = 0
+        elif len(axes) == 1:
+            axes = ((axes[0]),(0))
+        else:
+            axes = (tuple(axes),range(len(axes)))
+        
+        factor_msg_product = np.tensordot(self.f,msg_product, axes=axes)
+        
+        #Marginalize over all of the variables associated with the incoming messages
+#         print factor_msg_product.shape
+#         raw_input()
+#         marginals = np.sum(factor_msg_product, axis=0)
+        other.receive_msg(self,factor_msg_product)
+        self.pending.remove(other)
+           
+    def send_ms_msg(self, other):
+        # TODO: implement Factor -> Variable message for max-sum
+        pass
---
+++ lab2/main.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import Node
+
+def sum_product(node_list):
+    for node in node_list:
+        pending = set(node.pending)
+        for other in pending:
+            node.send_sp_msg(other)        
+
+# Instantiate Variable nodes
+I = Node.Variable('Influenza',2)
+SM= Node.Variable('Smokes',2)
+ST= Node.Variable('SoreThroat',2)
+F = Node.Variable('Fever',2)
+B = Node.Variable('Bronchitis',2)
+C = Node.Variable('Coughing',2)
+W = Node.Variable('Wheezing',2)
+
+# and Factor nodes
+I_prior = Node.Factor('I_prior',np.array([0.05,0.95]),[I])
+SM_prior = Node.Factor('SM_prior',np.array([0.2,0.8]),[SM])
+
+I_ST  = Node.Factor('I_ST',np.empty((2,2)),[I, ST])
+I_F   = Node.Factor('I_F',np.empty((2,2)),[I,F])
+SMI_B  = Node.Factor('SMI_B',np.empty((2,2,2)),[SM,I,B])
+B_CW  = Node.Factor('B_CW',np.empty((2,2,2)),[B,C,W])
+
+#add priors
+I_ST.f[1,1] = 0.3
+I_ST.f[1,0] = 0.7
+I_ST.f[0,1] = 0.001
+I_ST.f[0,0] = 0.999
+
+I_F.f[1,1] = 0.9
+I_F.f[1,0] = 0.1
+I_F.f[0,1] = 0.05
+I_F.f[0,0] = 0.95
+
+SMI_B.f[1,1,1] = 0.99
+SMI_B.f[1,1,0] = 0.01
+SMI_B.f[1,0,1] = 0.9
+SMI_B.f[1,0,0] = 0.1
+SMI_B.f[0,1,1] = 0.7
+SMI_B.f[0,1,0] = 0.3
+SMI_B.f[0,0,1] = 0.0001
+SMI_B.f[0,0,0] = 0.9999
+
+B_CW.f[1,1,1] = 0.8*0.6
+B_CW.f[1,0,1] = 0.2*0.6
+B_CW.f[0,1,1] = 0.07*0.001
+B_CW.f[0,0,1] = 0.93*0.001
+B_CW.f[1,1,0] = 0.8*0.4
+B_CW.f[0,1,0] = 0.07*0.999
+B_CW.f[1,0,0] = 0.2*0.4
+B_CW.f[0,0,0] = 0.93*0.999
+
+#Wheezing is root!
+I_prior.pending.update([I])
+SM_prior.pending.update([SM])
+ST.pending.update([I_ST])
+F.pending.update([I_F])
+C.pending.update([B_CW])
+
+node_list = [I_prior, SM_prior, ST, F, C, I_ST, I_F, I, SM, SMI_B, B, B_CW, W]
+sum_product(node_list)
+
+W.pending.update([B_CW])
+
+sum_product(reversed(node_list))
+
+for node in node_list:
+    if node.__class__.__name__ == "Variable":
+        marginal, Z = node.marginal(None)
+        print marginal
